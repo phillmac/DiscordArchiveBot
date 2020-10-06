e6a928009b83b421ab536ae35fceda5dcbdb9420
phillmac <phillmac@users.noreply.github.com>
2020-10-06 22:31:58 +1100

    Use async await

--- commands/archive/uploadqlist.js
+++ commands/archive/uploadqlist.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando')
 
 module.exports = class MeowCommand extends Command {
-  constructor(client) {
+  constructor (client) {
     super(client, {
       name: 'uploadqlist',
       aliases: ['upload-queue-list', 'uql'],
@@ -12,9 +12,11 @@ module.exports = class MeowCommand extends Command {
     })
   }
 
-  run(message) {
+  async run (message) {
     this.client.archive.uploadQueue.ensure(message.guild.id, [])
+    const uploadQueue = new Set()
+    await (this.client.archive.uploadQueue.get(message.guild.id)).forEach(item => uploadQueue.add(item))
     message.say('Upload queue contents:')
-    return message.say(JSON.stringify(this.client.archive.uploadQueue.get(message.guild.id), null, 2))
+    return message.say(JSON.stringify(uploadQueue, null, 2))
   }
 }
