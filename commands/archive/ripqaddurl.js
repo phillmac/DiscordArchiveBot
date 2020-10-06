51a11c4590a405f395a14ada18795fc94ca9737d
phillmac <phillmac@users.noreply.github.com>
2020-10-07 10:44:28 +1100

    Add ripqaddurl command

---
+++ commands/archive/ripqaddurl.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando')
+const fetch = require('node-fetch')
+const logger = require('winston')
+
+if (!process.env.RIPER_QUEUE_ADD_U_URL) {
+  throw new Error('RIPER_QUEUE_ADD_U_URL is required')
+}
+
+module.exports = class RipperQueueAddUrlCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ripqaddurl',
+      aliases: ['rip-queue-add-url', 'rqau'],
+      group: 'archive',
+      memberName: 'ripqaddurl',
+      description: 'Enques an existing gallery to be ripped',
+      guildOnly: true,
+      args: [
+        {
+          key: 'url',
+          prompt: 'url of gallery to add to the rip queue',
+          type: 'string'
+        }
+      ]
+    })
+  }
+
+  async run (message, { url }) {
+    const resp = await fetch(process.env.RIPER_QUEUE_ADD_U_URL, {
+      method: 'post',
+      body: `url=${url}`
+    })
+    const msg = await resp.text()
+    logger.debug({ url })
+    return message.say(`Added ${url} to ripper queue`)
+  }
+}
