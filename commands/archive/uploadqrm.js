44795e3975651e20601dce60db5cef8dd2fc4039
phillmac <phillmac@users.noreply.github.com>
2020-10-07 11:40:07 +1100

    Add uploadqrm command

---
+++ commands/archive/uploadqrm.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando')
+
+module.exports = class UploadQueueAddCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'uploadqrm',
+      aliases: ['upload-queue-remove', 'uqr'],
+      group: 'archive',
+      memberName: 'uploadqrm',
+      description: 'Removes a gallery from the archive upload queue',
+      guildOnly: true,
+      args: [
+        {
+          key: 'galleryName',
+          prompt: 'Name of gallery to remove from the upload queue',
+          type: 'string'
+        },
+        {
+          key: 'galleryType',
+          prompt: 'Type of gallery to remove from the upload queue',
+          type: 'string',
+          oneOf: ['gallery', 'favs']
+        }
+      ]
+    })
+  }
+
+  run (message, { galleryName, galleryType }) {
+    this.client.archive.uploadQueue.ensure(message.guild.id, [])
+    this.client.archive.uploadQueue.remove(message.guild.id, { galleryName, galleryType })
+    return message.say(`Removed ${galleryName} ${galleryType} from upload queue`)
+  }
+}
