db55189aa225e183dac62965c00bb8dab0d2c56e
phillmac <phillmac@users.noreply.github.com>
2020-10-21 11:30:44 +1100

    Add error handling

--- commands/archive/ripqquery.js
+++ commands/archive/ripqquery.js
@@ -1,13 +1,13 @@
 const { Command } = require('discord.js-commando')
 const fetch = require('node-fetch')
-// const logger = require('winston')
+const logger = require('winston')
 
 if (!process.env.RIPER_QUEUE_QUERY_URL) {
   throw new Error('RIPER_QUEUE_QUERY_URL is required')
 }
 
 module.exports = class RipperQueueAddCommand extends Command {
-  constructor (client) {
+  constructor(client) {
     super(client, {
       name: 'ripqquery',
       aliases: ['rip-queue-query', 'rqq'],
@@ -25,17 +25,24 @@ module.exports = class RipperQueueAddCommand extends Command {
     })
   }
 
-  async run (message, { galleryName }) {
-    const resp = await fetch(process.env.RIPER_QUEUE_QUERY_URL)
-    const matches = await (resp.json())
-      .map(qi => Object.fromEntries(qi))
-      .filter(qi => qi?.deviant?.toLowerCase() === galleryName.toLowerCase())
+  async run(message, { galleryName }) {
+    try {
+      const resp = await fetch(process.env.RIPER_QUEUE_QUERY_URL)
+      if (resp.status === 200)
+        const matches = await (resp.json())
+          .map(qi => Object.fromEntries(qi))
+          .filter(qi => qi?.deviant?.toLowerCase() === galleryName.toLowerCase())
 
-    if (matches.length === 0) {
-      return message.say(` No queue items found for \`${galleryName}\`.`)
+      if (matches.length === 0) {
+        return message.say(` No queue items found for \`${galleryName}\`.`)
+      }
+      return message.say(matches
+        .map(m => `\`Name: ${m?.deviant} Mode: ${m?.mode} priority: ${m?.priority}\``)
+        .join('\n')
+      )
+    } catch (err) {
+      logger.error(err)
     }
-    for (const qItem of matches) {
-      message.say(`\`Name: ${qItem?.deviant} Mode: ${qItem?.mode} priority: ${qItem?.priority}\``)
-    }
+    return message.say('Unable to query queue manager')
   }
 }
