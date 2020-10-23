d248ce09b719e3a7937a77822fd62ec300463656
phillmac <phillmackintosh@gmail.com>
2020-10-23 07:52:04 +0000

    Add commands

---
+++ commands/archive/ripqaddsub.js
@@ -0,0 +1,54 @@
+const { Command } = require('discord.js-commando')
+const fetch = require('node-fetch')
+const logger = require('winston')
+
+if (!process.env.RIPER_QUEUE_ADD_URL) {
+  throw new Error('RIPER_QUEUE_ADD_URL is required')
+}
+
+module.exports = class RipperQueueAddSubCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ripqaddsub',
+      aliases: ['rip-queue-add', 'rqas'],
+      group: 'archive',
+      memberName: 'ripqaddsub',
+      description: 'Enques an existing gallery to be ripped',
+      guildOnly: false,
+      args: [
+        {
+          key: 'galleryName',
+          prompt: 'Name of gallery to add to the rip queue',
+          type: 'string'
+        },
+        {
+          key: 'galleryType',
+          prompt: 'Type of gallery to add to the rip queue',
+          type: 'string',
+          oneOf: ['album', 'collection']
+        },
+        {
+          key: 'subType',
+          prompt: 'Sub type of gallery to add to the rip queue',
+          type: 'string'
+        }
+      ]
+    })
+  }
+
+  hasPermission (msg) {
+    if (!this.client.isOwner(msg.author)) return 'Only the bot owner(s) may use this command.'
+    return true
+  }
+
+  async run (message, { galleryName, galleryType, subType }) {
+    const resp = await fetch(process.env.RIPER_QUEUE_ADD_URL, {
+      method: 'POST',
+      body: JSON.stringify([{ deviant: galleryName, mode: galleryType, mval: subType, priority: 110 }]),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const status = await resp.text()
+    logger.debug({ galleryName, galleryType, subType, status })
+    return message.say(`Added \`${galleryName} ${galleryType} ${subType}\` to ripper queue. Status: ${status}`)
+  }
+}
---
+++ commands/archive/ripqaddsubfull.js
@@ -0,0 +1,59 @@
+const { Command } = require('discord.js-commando')
+const fetch = require('node-fetch')
+const logger = require('winston')
+
+if (!process.env.RIPER_QUEUE_ADD_URL) {
+  throw new Error('RIPER_QUEUE_ADD_URL is required')
+}
+
+module.exports = class RipperQueueAddSubFullCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ripqaddsubfull',
+      aliases: ['rip-queue-add-sub-full', 'rqasf'],
+      group: 'archive',
+      memberName: 'ripqaddsubfull',
+      description: 'Enques an existing gallery to be ripped, forcing a full crawl',
+      guildOnly: false,
+      args: [
+        {
+          key: 'galleryName',
+          prompt: 'Name of gallery to add to the rip queue',
+          type: 'string'
+        },
+        {
+          key: 'galleryType',
+          prompt: 'Type of gallery to add to the rip queue',
+          type: 'string',
+          oneOf: ['album', 'collection']
+        },
+        {
+          key: 'subType',
+          prompt: 'Sub type of gallery to add to the rip queue',
+          type: 'string'
+        },
+        {
+          key: 'priority',
+          prompt: 'Priority to assign job in the rip queue',
+          type: 'integer'
+        }
+      ]
+    })
+  }
+
+  hasPermission (msg) {
+    if (!this.client.isOwner(msg.author)) return 'Only the bot owner(s) may use this command.'
+    return true
+  }
+
+  async run (message, { galleryName, galleryType, priority }) {
+    const resp = await fetch(process.env.RIPER_QUEUE_ADD_URL, {
+      method: 'POST',
+      body: JSON.stringify([{ deviant: galleryName, mode: galleryType, priority, full_crawl: true }]),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const status = await resp.text()
+    logger.debug({ galleryName, galleryType, priority, status })
+    return message.say(`Added \`${galleryName} ${galleryType} ${priority}\` to ripper queue with forced full crawl. Status: ${status}`)
+  }
+}
